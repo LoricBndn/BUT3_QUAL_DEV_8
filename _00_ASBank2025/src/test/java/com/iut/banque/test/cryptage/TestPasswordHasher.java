92f0bee62bd46bf5420a2dfe12530db75049710b
Théo <theo.schaller57@gmail.com>
2025-09-26 09:31:02 +0200

    ✅  update Test PasswordHasher

--- _00_ASBank2025/src/test/java/com/iut/banque/test/cryptage/TestPasswordHasher.java
+++ _00_ASBank2025/src/test/java/com/iut/banque/test/cryptage/TestPasswordHasher.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class TestPasswordHasher {
 
     @Test
-    public void testHashPassword() {
+    public void testHashPasswordSucceed() {
         String motDePasse = "MonSuperMotDePasse";
         Assert.assertTrue(PasswordHasher.verifyPassword(motDePasse, PasswordHasher.hashPassword(motDePasse)));
+    }
+
+    @Test
+    public void testHashPasswordFailed() {
+        String motDePasse = "MonSuperMotDePasse";
+        Assert.assertFalse(PasswordHasher.verifyPassword(motDePasse, PasswordHasher.hashPassword("MonMotDePasseEstNul")));
     }
 }
